72ba9545fba3af9ce77432b65cc8de09b2b544df
robbiemurphy <rmurphy987@gmail.com>
2022-06-01 04:11:34 -0500

    server.js altered, invoking starting Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
+const {ApolloServer} = require('apollo-server-express');
+const {authMiddleware} = require('./utils/auth');
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +22,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer(typeDefs, resolvers);
